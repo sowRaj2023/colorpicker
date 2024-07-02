96b0fb5096b37b11942a31d77f6f33b962816ed5
sowRaj2023 <sownyuvan1180@gmail.com>
2024-07-02 20:13:48 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,22 @@
+const mainContainerEl = document.getElementById("mainContainer");
+const spanEls = document.getElementById("spanEl");
+
+function button1(){
+    mainContainerEl.style.backgroundColor = "#e0e0e0";
+    spanEls.textContent = "#e0e0e0"
+}
+
+function button2(){
+    mainContainerEl.style.backgroundColor = "#6fcf97";
+    spanEls.textContent = "#6fcf97"
+}
+
+function button3(){
+    mainContainerEl.style.backgroundColor = "#56ccf2";
+    spanEls.textContent = "#56ccf2"
+}
+
+function button4(){
+    mainContainerEl.style.backgroundColor = "#bb6bd9";
+    spanEls.textContent = "#bb6bd9"
+}
